387b35850e27c5ed63aa356906e064288015bc46
FMaslina <FMaslina@yandex.ru>
2024-12-03 13:56:51 +0700

    Add Task Schemas

---
+++ schemas/__init__.py
@@ -0,0 +1,1 @@
+from schemas.task import TaskRead, TaskCreate, TaskUpdate
---
+++ schemas/task.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+from models.task import TaskStatus
+
+
+class TaskBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+    status: TaskStatus = TaskStatus.todo
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskUpdate(TaskBase):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    status: Optional[TaskStatus] = None
+
+
+class TaskRead(TaskBase):
+    id: int
+    created_at: datetime
+    updated_at: Optional[datetime] = None
+
+    class Config:
+        from_attributes = True
